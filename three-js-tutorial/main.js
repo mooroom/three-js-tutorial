7bccff6aa04ecfae20b2cd877ed444abd81c5f24
mooroom <61869246+mooroom@users.noreply.github.com>
2021-07-06 21:24:42 +0900

    0706
    camera, plane, light test

--- three-js-tutorial/main.js
+++ three-js-tutorial/main.js
@@ -1,6 +1,44 @@
-import './style.css'
+import * as THREE from "https://cdn.skypack.dev/three@0.126.1";
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  innerWidth / innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGLRenderer();
+
+console.log(scene);
+console.log(camera);
+console.log(renderer);
+
+renderer.setSize(innerWidth, innerHeight);
+renderer.setPixelRatio(devicePixelRatio);
+document.body.appendChild(renderer.domElement);
+
+camera.position.z = 5;
+
+const planeGeometry = new THREE.PlaneGeometry(5, 5, 10, 10);
+const planeMaterial = new THREE.MeshPhongMaterial({
+  color: 0xff0000,
+  side: THREE.DoubleSide,
+});
+
+const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
+scene.add(planeMesh);
+
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(0, 0, 1);
+scene.add(light);
+
+console.log(planeMesh);
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+  // planeMesh.rotation.x += 0.01;
+}
+
+animate();
