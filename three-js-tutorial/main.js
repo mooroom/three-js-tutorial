b07eaf828289de9ae1e9ccd2bace2706ad5e0f0b
mooroom <61869246+mooroom@users.noreply.github.com>
2021-07-07 20:48:56 +0900

    0707
    -dat.gui added
    -orbit controls added

--- three-js-tutorial/main.js
+++ three-js-tutorial/main.js
@@ -1,4 +1,43 @@
 import * as THREE from "https://cdn.skypack.dev/three@0.126.1";
+import { OrbitControls } from "https://cdn.skypack.dev/three@0.126.1/examples/jsm/controls/OrbitControls.js";
+
+console.log(OrbitControls);
+
+import * as dat from "dat.gui";
+console.log(dat);
+
+const gui = new dat.GUI();
+const world = {
+  plane: {
+    width: 10,
+    height: 10,
+    widthSegments: 10,
+    heightSegments: 10,
+  },
+};
+gui.add(world.plane, "width", 1, 20).onChange(generatePlane);
+gui.add(world.plane, "height", 1, 20).onChange(generatePlane);
+gui.add(world.plane, "widthSegments", 1, 50).onChange(generatePlane);
+gui.add(world.plane, "heightSegments", 1, 50).onChange(generatePlane);
+
+function generatePlane() {
+  planeMesh.geometry.dispose();
+  planeMesh.geometry = new THREE.PlaneGeometry(
+    world.plane.width,
+    world.plane.height,
+    world.plane.widthSegments,
+    world.plane.heightSegments
+  );
+
+  const { array } = planeMesh.geometry.attributes.position;
+  for (let i = 0; i < array.length; i += 3) {
+    const x = array[i];
+    const y = array[i + 1];
+    const z = array[i + 2];
+
+    array[i + 2] = z + Math.random();
+  }
+}
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -18,12 +57,14 @@ renderer.setSize(innerWidth, innerHeight);
 renderer.setPixelRatio(devicePixelRatio);
 document.body.appendChild(renderer.domElement);
 
+new OrbitControls(camera, renderer.domElement);
 camera.position.z = 5;
 
 const planeGeometry = new THREE.PlaneGeometry(5, 5, 10, 10);
 const planeMaterial = new THREE.MeshPhongMaterial({
   color: 0xff0000,
   side: THREE.DoubleSide,
+  flatShading: THREE.FlatShading,
 });
 
 const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
@@ -33,7 +74,9 @@ const light = new THREE.DirectionalLight(0xffffff, 1);
 light.position.set(0, 0, 1);
 scene.add(light);
 
-console.log(planeMesh);
+const backLight = new THREE.DirectionalLight(0xffffff, 1);
+backLight.position.set(0, 0, -1);
+scene.add(backLight);
 
 function animate() {
   requestAnimationFrame(animate);
